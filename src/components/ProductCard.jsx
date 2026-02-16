e97383b1ad8111346107019cad2386308b35af80
MuthomiKaburu <kabmuthomi01@gmail.com>
2026-02-16 22:47:29 +0300

    changes made

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import styles from "../styles/ProductCard.module.css";
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, onRemove }) => {
   return (
-    <div className={product.inStock ? styles.card : `${styles.card} ${styles.outOfStock}`}>
+    <div
+      className={
+        product.inStock
+          ? styles.card
+          : `${styles.card} ${styles.outOfStock} outOfStockClass`
+      }
+    >
       {product.inStock ? <p>in stock</p> : <p>out of stock</p>}
+
       <h2>{product.name}</h2>
       <h3>{product.price}</h3>
 
-      
+      <button onClick={() => onRemove(product.id)}>
+        Remove
+      </button>
     </div>
   );
 };
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ProductCard from './ProductCard';
 
-const ProductList = ({ products }) => {
-  // TODO: Check if the product list is empty and display a message if needed
-  if(!products){
-    return <p>No products found</p>
-    
+const ProductList = ({ products, onRemove }) => {
+
+  if (products.length === 0) {
+    return <p>No products found</p>;
   }
+
   return (
     <div>
-      {/* TODO: Iterate over the products array and render a ProductCard for each product */}
       {products.map((product) => (
-<ProductCard key={product.id} product={product} />
-))}
+        <ProductCard
+          key={product.id}
+          product={product}
+          onRemove={onRemove}
+        />
+      ))}
     </div>
   );
 };
